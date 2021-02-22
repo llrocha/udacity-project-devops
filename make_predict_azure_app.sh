b732e2d389d72620df6d9cb3b84c4ac2c8663961
Leonardo Larsen Rocha <brhbs0305@ambev.com.br>
2021-02-22 08:50:50 -0300

    Change app name

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://leonardolpi-udacity-app.azurewebsites.net:$PORT/predict 
+     -X POST https://webapp-udacity-nunix.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
